8dde16b35006193c8d723005aa465aab7c997165
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-14 11:44:38 -0700

    Added test file

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 'use strict';
 
-
+//Initialize express, mongoose & middleware.
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+
 const app = express();
+
+//Send static files to client
 app.use(express.static('public'));
+
+
+//Port functions, can replace with runServer/closeServer functions
 app.listen(process.env.PORT || 8080);
+
+module.exports = app; // this if for testing
---
+++ test/test-movies.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const {app} = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Movies', function() {
+  it('should load HTML file', function() {
+    return chai.request(app)
+      .get('/')
+      .then(function(res) {
+        (res).should.have.status(200);
+      });
+  });
+});
