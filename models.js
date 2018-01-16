3014409c32b4274ab5603825fd5d7a6e564d3dcf
Jeff Connelly <jeff.connelly6@gmail.com>
2018-01-16 09:26:03 -0700

    Added all endpoints to work with favorites collection

--- models.js
+++ models.js
@@ -17,7 +17,17 @@ const movieSchema = mongoose.Schema({
 
 //Create schema for Box Office
 const favoritesSchema = mongoose.Schema({
-
+  title: String,
+  release_date: String,
+  poster_path: String,
+  overview: String,
+  vote_average: Number,
+  netflix: Boolean,
+  amazon: Boolean,
+  hbo: Boolean,
+  hulu: Boolean,
+  comment: String,
+  user_rating: Number,
 });
 
 movieSchema.methods.serialize = function() {
@@ -34,6 +44,24 @@ movieSchema.methods.serialize = function() {
   };
 };
 
+favoritesSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    title: this.title,
+    poster_path: this.poster_path,
+    overview: this.overview,
+    vote_average: this.vote_average,
+    netflix: this.netflix,
+    amazon: this.amazon,
+    hbo: this.hbo,
+    hulu: this.hulu
+  };
+};
+
 const Movies = mongoose.model('Movies', movieSchema);
 
+const Favorites = mongoose.model('Favorites', favoritesSchema);
+
 module.exports = {Movies};
+module.exports = {Favorites};
+
--- routers/movies.router.js
+++ routers/movies.router.js
@@ -6,6 +6,7 @@ const express = require('express');
 const router = express.Router();
 const { DATABASE_URL, PORT } = require('../config');
 const { Movies }  = require('../models.js');
+const { Favorites } = require('../models.js');
 
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -27,7 +28,7 @@ router.get('/', (req, res) => {
     })
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'something went terribly wrong' });
+      res.status(500).json({ error: 'something went wrong' });
     });
 });
 
@@ -37,20 +38,65 @@ router.get('/:id', (req, res) => {
     .then(movie => res.json(movie.serialize()))
     .catch(err => {
       console.error(err);
-      res.status(500).json({ error: 'something went horribly awry' });
+      res.status(500).json({ error: 'something went wrong' });
     });
 });
 
 router.post('/', (req, res) => {
- 
+  const requiredFields = ['title', 'overview', 'poster_path', 'vote_average', 'netflix', 'amazon', 'hbo', 'hulu'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Favorites
+    .create({
+      title: req.body.title,
+      poster_path: req.body.poster_path,
+      overview: req.body.overview,
+      vote_average: req.body.vote_average,
+      netflix: req.body.netflix,
+      amazon: req.body.amazon,
+      hbo: req.body.hbo,
+      hulu: req.body.hulu,
+      comment: req.body.comment,
+      user_rating: req.body.user_rating})
+    .then(
+      movies => res.status(201).json(movies.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
 });
 
 router.put('/:id', (req, res) => {
- 
+  //Need to update comments. Do we need a 
+  //comments property, or other properties?
+  //Create comments and star rating properties in DB. 
+  const updateObj = {};
+  const updateableFields = ['comment', 'user_rating'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  Favorites
+    .findByIdAndUpdate(req.params.id, {$set: updateObj})
+    .then(movies => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 router.delete('/:id', (req, res) => {
- 
+  Favorites
+    .findByIdAndRemove(req.params.id)
+    .then(() => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 
